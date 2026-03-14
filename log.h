933bd3a5fa30a79255ba9d60ec53e8e14e8175c0
Reynald Bartiana <reynaldbartiana@gmail.com>
2026-03-14 16:10:07 +0800

    Add log.h with modem and ADXL345 configurations
    log cpp utils

---
+++ log.h
@@ -0,0 +1,23 @@
+#pragma once
+/**
+ * @file      utilities.h
+ * @author    reynald bartiana
+ * @license   MIT
+ * @date      2026-03-14
+ *
+ */
+
+ 
+// LILYGO_T_A7608X_S3
+#define MODEM_BAUD_RATE       115200
+#define MODEM_TX_PIN          16
+#define MODEM_RX_PIN          17
+#define MODEM_POWER_PIN       4
+#define MODEM_RESET_PIN       NULL
+
+// ADXL345
+#define ADXL345_ADDR          0x53
+#define ADXL345_SDA           21
+#define ADXL345_SCL           22
+#define ADXL345_INTP1         NULL
+#define ADXL345_INTP2         NULL
